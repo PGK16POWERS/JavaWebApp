904020fbe70b09fca0c744d6e63f17a8595b1ce2
Liberia (Kaiberius) Kartier <136324051+PGK16POWERS@users.noreply.github.com>
2024-03-26 11:48:11 +0200

    Update HelloController.java

--- hello/src/main/java/com/hello/hello/HelloController.java
+++ hello/src/main/java/com/hello/hello/HelloController.java
@@ -4,15 +4,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.util.StreamUtils;
 
 import java.nio.charset.StandardCharsets;
+import java.io.IOException;
 
 @RestController
 public class HelloController {
 
     @GetMapping("/")
-    public String home() {
-        return "Something Nyana";
+    public String home() throws IOException {
+        Resource resource = new ClassPathResource("static/index.html");
+
+        String htmlContent = StreamUtils.copyToString(resource.getInputStream(), StandardCharsets.UTF_8);
+
+        return htmlContent;
     }
 
     @GetMapping("/login")
